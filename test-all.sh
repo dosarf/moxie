eebbaf55a3764f88b1bf9909026602c44a1f67ff
Ferenc Dosa-Racz <dosarf@gmail.com>
2021-02-28 18:39:28 +0200

    Exclude folder pgdata/ from tests collation
    - since it will have restricted access (making pytest fail)
      and it does not have tests anyway

--- test-all.sh
+++ test-all.sh
@@ -6,8 +6,8 @@ do
   for schema_creation in orm migrate
   do
     # echo $schema_creation
-    echo PYTHONPATH=src:. pytest --db-server=$db_server --schema-creation=$schema_creation
-    PYTHONPATH=src:. pytest --db-server=$db_server --schema-creation=$schema_creation
+    echo PYTHONPATH=src:. pytest --db-server=$db_server --schema-creation=$schema_creation tests
+    PYTHONPATH=src:. pytest --db-server=$db_server --schema-creation=$schema_creation tests
     [ $? -eq 0 ] || exit 1
   done
 done
